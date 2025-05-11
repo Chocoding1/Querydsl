6bca5f99532972e6fd917b2de9322082199582e4
chocoding <galmeagi2@naver.com>
2025-05-11 17:04:10 +0900

    예제 도메인 모델 테스ㅌ

--- src/test/java/study/querydsl/entity/MemberTest.java
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -4,12 +4,10 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
