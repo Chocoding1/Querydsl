88dc557a60791f4bed2d91a50964069122e699a1
chocoding <galmeagi2@naver.com>
2025-06-16 17:37:15 +0900

    사용자 정의 인터페이스 구현체명 변경

--- src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
@@ -5,7 +5,6 @@ import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
@@ -21,12 +20,17 @@ import static study.querydsl.entity.QTeam.team;
 /**
  * 클래스명은 Spring Data interface 이름 뒤에 Impl을 붙인 형태로 작성해야 한다.(의무)
  * 그래야 스프링 데이터 JPA가 인식해서 스프링 빈으로 등록한다.
+ *
+ * +) 스프링 데이터 2.x부터는 사용자 정의 인터페이스명 + Impl방식도 지원한다.
+ * 기존 방식보다 이 방식이 사용자 정의 인터페이스 이름과 구현 클래스 이름이 비슷하므로 더 직관적이다.
+ * 추가로 여러 인터페이스를 분리해서 구현하는 것도 가능하기 때문에 새롭게 변경된 이 방식을 사용하는 것을 더 권장한다.
+ * MemberRepositoryImpl -> MemberRepositoryCustomImpl 변경
  */
-public class MemberRepositoryImpl implements MemberRepositoryCustom{
+public class MemberRepositoryCustomImpl implements MemberRepositoryCustom{
 
     private final JPAQueryFactory queryFactory;
 
-    public MemberRepositoryImpl(EntityManager em) {
+    public MemberRepositoryCustomImpl(EntityManager em) {
         this.queryFactory = new JPAQueryFactory(em);
     }
 
